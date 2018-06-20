d6ca674cf99f890bd00a0c790af2b036339a1f82
bskipper <baskipper67@gmail.com>
2018-06-20 10:02:45 -0500

    created basic store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {stack as rootReducer} from "./reducers";
 import App from "./components/App";
 import Stack from "./components/Stack"
+import {setStack} from "./actions";
+
+const store = createStore(rootReducer);
+store.subscribe(() => {
+
+});
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route exact path="/stack" component={Stack} />
-        </Switch>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={App}/>
+                <Route exact path="/stack" component={Stack}/>
+            </Switch>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root'));
