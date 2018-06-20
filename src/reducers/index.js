645a4089e19ab25b3ed4f7326b4321c3271ed31e
bskipper <baskipper67@gmail.com>
2018-06-20 09:51:37 -0500

    created basic reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import {} from 'react-redux';
+import {SET_STACK} from "../actions/types";
+
+const INITIAL_STATE = {};
+
+export const stack = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case SET_STACK:
+            return [...state, action.stack];
+        default:
+            return state;
+    }
+};
