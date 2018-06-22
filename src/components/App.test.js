8ce48dbfec954400c660af8f2269393a5e4272f8
bskipper <baskipper67@gmail.com>
2018-06-22 07:59:48 -0500

    Tested app rendering

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,7 +4,17 @@ import App from './App';
 
 describe('App', () => {
     const app = shallow(<App />);
+
     it('renders the `FlashCard Pro` title', () => {
         expect(app.find('h2').text()).toEqual('FlashCard Pro')
+    });
+
+    it('renders the stack list', () => {
+       expect(app.find('Connect(StackList)').exists()).toBe(true);
+    });
+
+    it('renders a link to create new stacks', () => {
+        expect(app.find('Link h4').text()).toEqual('Create a new stack')
     })
+
 });
