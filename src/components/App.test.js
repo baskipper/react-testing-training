72142e5a30c49dde20338623dde0b6389ae3f9ef
bskipper <baskipper67@gmail.com>
2018-06-22 07:46:15 -0500

    Wrote testing skeleton

---
+++ src/components/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+    const app = shallow(<App />);
+    it('renders the `FlashCard Pro` title', () => {
+        expect(app.find('h2').text()).toEqual('FlashCard Pro')
+    })
+});
